f817cd98dee91ead0a64c3c50e3b9bbe34cc62a9
SongShuHao <ssh20000511@SoHu.com>
2022-01-20 19:56:36 +0800

    finish main

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ extern int w;
 
 int main()
 {
-    FILE* f = fopen("data.dbf", "w");
+    FILE *f = fopen("data.dbf", "w");
     fclose(f);
     BMgr BufManager, Init;
     for (int i = 0; i < DEFBUFSIZE; i++)
@@ -21,10 +21,31 @@ int main()
         BufManager.UnfixPage(np.page_id);
     }
     Init.storage.CloseFile();
-    cout<<"hit:"<<hit<<"  miss:"<<miss<<endl;
+    cout << "hit:" << hit << "  miss:" << miss << endl;
     hit = 0;
     miss = 0;
     r = 0;
     w = 0;
-    
+    FILE *file2 = fopen("data-5w-50w-zipf.txt", "r");
+    //    FILE* file2 = fopen("t2.txt", "r");
+    int operation;
+    int page;
+    int frame_id;
+    int i = 0;
+    while (fscanf(file2, "%d,%d", &operation, &page) != EOF)
+    {
+        i++;
+        page -= 1;
+        frame_id = BufManager.FixPage(page, 0);
+        if (operation == 1)
+        {
+            //if write, set frame to dirty
+            BufManager.SetDirty(frame_id);
+        }
+        BufManager.UnfixPage(page);
+    }
+    BufManager.WriteDirtys();
+    BufManager.storage.CloseFile();
+    cout << "total tests:" << i << "\nhit: " << hit << " miss:" << miss << " read:" << r << " write:" << w << endl;
+    return 0;
 }
